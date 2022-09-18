548501101f61dd996d53407e85f77e97374c03c9
Michalina Graczyk <michalina.graczyk@saleor.io>
2022-09-18 15:36:30 +0200

    Add puppeteer to verify if the application starts

--- test/cli-with-apps/app-create/create-app.test.ts
+++ test/cli-with-apps/app-create/create-app.test.ts
@@ -1,23 +1,25 @@
-import {test, beforeEach} from "vitest";
-import {$, cd} from 'zx'
+import {test, beforeEach, expect} from "vitest";
+import {$, sleep} from 'zx'
 const rimraf = require('rimraf')
 import fkill from 'fkill';
 import puppeteer from 'puppeteer'
 
 beforeEach(async () => {
     rimraf.sync('test-app')
-    // await fkill(":3000")
+    await fkill(":3000")
 })
 
 test('CLI can create and run app-template',async () =>{
     await $`saleor app create test-app --json --deps false`
     await $`cd test-app && pnpm i`
-    // fails
-    await $.spawn(`cd test-app && pnpm dev`)
+    $`cd test-app && pnpm dev`
 
-    console.log(await $`curl localhost:3000`)
-
+    await sleep(2000)
     // open puppeteer & test if app created
-
-    // cleanup
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto('http://localhost:3000')
+    const text = await page.$eval('#__next', el => el.textContent)
+    expect(text).toContain('Saleor Dashboard')
+    await browser.close()
 });
